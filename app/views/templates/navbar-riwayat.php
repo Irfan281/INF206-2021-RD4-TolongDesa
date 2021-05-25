72379a6c2ddaf28645dee80231f9b90fd5cf621c
mufidakhbar99 <mufid.a@mhs.unsyiah.ac.id>
2021-05-25 21:34:41 +0700

    Menambahkan Navbar Untuk Halaman Riwayat

---
+++ app/views/templates/navbar-riwayat.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="<?= BASEURL; ?>/css/navbar.css">
+</head>
+
+<body>
+    <!--Navbar  -->
+    <nav class="navbar shadow-lg navbar-expand navbar-style fixed-bottom d-md-none d-lg-none d-xl-none">
+        <ul class="navbar-nav nav-justified w-100">
+            <li class="nav-item">
+                <a class="btn-grey fs-2" href="<?= BASEURL; ?>/home"><i class="bx bxs-home-circle"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="fs-2" href="<?= BASEURL; ?>/home/riwayat" style="color : #FF7133"><i class=" bx bx-history"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="btn-grey fs-2" href="<?= BASEURL; ?>/masyarakat/profil"><i class='bx bxs-user-account'></i></a>
+            </li>
+        </ul>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+
+</html>
